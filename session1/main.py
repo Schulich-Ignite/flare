1d3f61a564dc974106840d72830c581adeccbd18
R Lee <alpha.r.lee.dev@gmail.com>
2021-02-05 08:48:18 -0700

    Added screen.set_alpha again

--- session1/main.py
+++ session1/main.py
@@ -18,6 +18,7 @@ WHITE = (255, 255, 255)
 
 # Creating the screen and the clock
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen.set_alpha(0)  # Make alpha bits transparent
 clock = pygame.time.Clock()
 
 # Create our 50 x 100 rectangle at x = 0, y = 0
