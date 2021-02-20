45ab675b87ffcf56908d60a90882ddc71be9625a
anandp1 <73033746+anandp1@users.noreply.github.com>
2021-02-20 12:38:00 -0700

    Update main_COPY_AND_RENAME.py

--- main_COPY_AND_RENAME.py
+++ main_COPY_AND_RENAME.py
@@ -33,24 +33,25 @@ while True:
     # Keyboard events
     keys_pressed = pygame.key.get_pressed()
     if keys_pressed[pygame.K_UP]:
-        # Replace this line
+        pass  # Replace this line
     if keys_pressed[pygame.K_LEFT]:
-        # Replace this line
+        pass  # Replace this line
     if keys_pressed[pygame.K_RIGHT]:
-        # Replace this line
+        pass  # Replace this line
     if keys_pressed[pygame.K_DOWN]:
-        # Replace this line
-
+        pass  # Replace this line
 
     # Mouse events
-    mouse_buttons = pygame.mouse.get_pressed()
-    # Runs while you hold it
-    if mouse_buttons[0]:  # If left mouse pressed
-        mouse_pos = pygame.mouse.get_pos()  # Get position of mouse
-        # Returns (x, y) coordinates as tuple
+    mouse_pos = pygame.mouse.get_pos()  # Get position of mouse
+                                        # Returns (x, y) coordinates as tuple
 
+    mouse_buttons = pygame.mouse.get_pressed()
+    if mouse_buttons[0]:  # If left mouse pressed
+        pass  # Replace this line
     if mouse_buttons[2]:  # If right mouse pressed
-        # Replace this line
+        pass  # Replace this line
+
+
 
     """
     UPDATE section - manipulate everything on the screen
@@ -64,6 +65,6 @@ while True:
     screen.fill(BLACK)  # Fill the screen with one colour
 
 
+
     pygame.display.flip()  # Pygame uses a double-buffer, without this we see half-completed frames
     clock.tick(FRAME_RATE)  # Pause the clock to always maintain FRAME_RATE frames per second
-    
